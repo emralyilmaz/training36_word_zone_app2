5dd1669d31e290b420f0ae328f457e8ea34595a6
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-19 11:04:24 +0300

    Tarihi Formatlama İşlemi

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String konum;
@@ -17,7 +18,16 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       String offset = veri["utc_offset"].substring(1, 3);
       now = now.add(Duration(hours: int.parse(offset)));
-      zaman = now.toString();
+      zaman = DateFormat.jm().format(now); // zaman: 9:58 AM
+      // zaman = DateFormat.j().format(now); // zaman: 9 AM
+      // zaman = DateFormat.d().format(now); // zaman: 19 (ayın 19.günü)
+      // zaman = DateFormat.LLLL().format(now); // zaman: August(hangi ay olduğu tam)
+      // zaman = DateFormat.LLL().format(now); // zaman: Aug(hangi ay olduğu tam)
+      // zaman = DateFormat.yMMMd().format(now); // zaman: Aug 19, 2020
+      // zaman = DateFormat.yMd().format(now); // zaman: 8/19/2020
+      // zaman = DateFormat.Hm().format(now); // zaman: 10:02
+      zaman = DateFormat.EEEE().format(now); // zaman: Wednesday
+      zaman = DateFormat.E().format(now); // zaman: Wednesday
     } catch (e) {
       zaman = "zaman bilgisi getirilemedi.";
     }
