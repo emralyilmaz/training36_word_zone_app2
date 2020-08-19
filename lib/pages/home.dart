7e492db04b7f62d8f9b0af8ab40fd8720e66dab3
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-19 11:10:51 +0300

    Tarihi Gösterme İşlemi

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,15 +16,35 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: SafeArea(
           child: Center(
-        child: Column(
-          children: [
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/konum");
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text("Konum Düzenle"))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 120),
+          child: Column(
+            children: [
+              FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/konum");
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text("Konum Düzenle")),
+              SizedBox(height: 50.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    routeVeriler["konum"],
+                    style: TextStyle(fontSize: 25, letterSpacing: 2.0),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Text(
+                    routeVeriler["zaman"],
+                    style: TextStyle(fontSize: 50),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       )),
     );
