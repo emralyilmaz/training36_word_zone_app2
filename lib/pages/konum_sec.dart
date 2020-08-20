3be1286c1283adc5fa45f10ce5a25a7e8dccb064
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-20 10:36:31 +0300

    listview

--- lib/pages/konum_sec.dart
+++ lib/pages/konum_sec.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:training36_word_zone_app2/services/world_time.dart';
 
 class Konum extends StatefulWidget {
   @override
@@ -6,19 +7,39 @@ class Konum extends StatefulWidget {
 }
 
 class _KonumState extends State<Konum> {
+  List<WorldTime> konumlar = [
+    WorldTime(url: "Europe/Istanbul", konum: "Istanbul", bayrak: "Turkiye.jpg"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(0, 223, 252, 1),
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(52, 56, 56, 1),
-        title: Text("Konum Seç"),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: Center(
-        child: Text("Konum"),
-      ),
-    );
+        backgroundColor: Color.fromRGBO(0, 223, 252, 1),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(52, 56, 56, 1),
+          title: Text("Konum Seç"),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: ListView.builder(
+            itemCount: konumlar.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+                child: Card(
+                  child: ListTile(
+                    onTap: () {
+                      print(konumlar[index].konum);
+                    },
+                    title: Text(konumlar[index].konum),
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          AssetImage("assets/${konumlar[index].bayrak}"),
+                    ),
+                  ),
+                ),
+              );
+            }));
   }
 }
